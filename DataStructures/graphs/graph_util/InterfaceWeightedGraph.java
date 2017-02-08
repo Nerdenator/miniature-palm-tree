8ab4acbc2014a2da2b469290b45a76c17a97f672
Adina Stoica <adina.stoica@gmail.com>
2017-02-07 23:27:14 -0600

    Weighted Graph Interface

---
+++ DataStructures/graphs/graph_util/InterfaceWeightedGraph.java
@@ -0,0 +1,10 @@
+package graph_util;
+
+public interface InterfaceWeightedGraph<MyType> extends InterfaceUnweightedGraph<MyType> {
+
+	// add an edge with weight w between nodes x,y and y,x
+	public void addEdge(int x, int y, int w);
+
+	// get the weight of edge x,y
+	public int getWeight(int x, int y);
+}
