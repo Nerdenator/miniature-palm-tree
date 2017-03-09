5e6f34b7f6322d1b5633054d6a89ec178f61dba9
Adina Stoica <adina.stoica@gmail.com>
2017-03-08 20:36:45 -0600

    Added swaps

---
+++ Algorithms/other/swappingArrayElements/Swaps.java
@@ -0,0 +1,50 @@
+package swappingArrayElements;
+
+public class Swaps {
+
+	/**
+	 * Using additional auxiliary variable (normal way to do it)
+	 * 
+	 * @param a array
+	 * @param i first index to swap
+	 * @param j second index to swap
+	 */
+	public static void swapTemp(int[] a, int i, int j) {
+		if (i != j) {
+			int temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
+	}
+
+	/**
+	 * In-place using arithmetic
+	 * 
+	 * @param a array
+	 * @param i first index to swap
+	 * @param j second index to swap
+	 */
+	public static void swapMath(int[] a, int i, int j) {
+		if (i != j) {
+			a[i] = a[i] + a[j];
+			a[j] = a[i] - a[j];
+			a[i] = a[i] - a[j];
+		}
+	}
+
+	/**
+	 * In-place using XOR (true only when inputs differ)
+	 * 
+	 * @param a array
+	 * @param i first index to swap
+	 * @param j second index to swap
+	 */
+	public static void swapXOR(int[] a, int i, int j) {
+		if (i != j) {
+			a[i] = a[i] ^ a[j];
+			a[j] = a[i] ^ a[j];
+			a[i] = a[i] ^ a[j];
+		}
+	}
+
+}
---
+++ Algorithms/other/swappingArrayElements/SwapsTest.java
@@ -0,0 +1,46 @@
+package swappingArrayElements;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SwapsTest {
+	int[] A;
+	int[] Aswapped;
+	int posi,posj;
+	
+	@Before
+	public void setUp(){
+		A = new int[] {1,2,3,4,5};
+		posi = 1;
+		posj = 3;
+		Aswapped = new int[] {1,4,3,2,5};
+	}
+	
+	@Test
+	public void testSwapXOR() {
+		Swaps.swapXOR(A, posi, posi);
+		Assert.assertArrayEquals(A, A);
+		
+		Swaps.swapXOR(A, posi, posj);
+		Assert.assertArrayEquals(Aswapped, A);
+	}
+
+	@Test
+	public void testSwapTemp() {
+		Swaps.swapTemp(A, posi, posi);
+		Assert.assertArrayEquals(A, A);
+		
+		Swaps.swapTemp(A, posi, posj);
+		Assert.assertArrayEquals(Aswapped, A);
+	}
+	
+	@Test
+	public void testSwapMath() {
+		Swaps.swapMath(A, posi, posi);
+		Assert.assertArrayEquals(A, A);
+		
+		Swaps.swapMath(A, posi, posj);
+		Assert.assertArrayEquals(Aswapped, A);
+	}
+}
